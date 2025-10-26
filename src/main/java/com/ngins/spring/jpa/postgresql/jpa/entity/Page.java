ef94ceaa43cab09610e2122dd11e73bbc6b94bdb
JonginSeok <ngins7512@gmail.com>
2025-10-26 20:09:46 +0900

    update

--- src/main/java/com/ngins/spring/jpa/postgresql/jpa/entity/Page.java
+++ src/main/java/com/ngins/spring/jpa/postgresql/jpa/entity/Page.java
@@ -9,9 +9,12 @@ import java.util.List;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -39,8 +42,10 @@ public class Page {
 	@Column(name = "name", length = 120, nullable = false, comment = "이름")
 	private String name;
 
-	@Column(name = "menu_id", comment = "메뉴ID")
-	private Long menu_id;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "menu_id", comment = "메뉴ID")
+	private Menu menu_id;
 
 	@OneToMany(mappedBy = "page_id", cascade = CascadeType.ALL, orphanRemoval = true) // 외래키 설정
 	private List<PageAction> pageActions = new ArrayList<PageAction>();
--- src/main/java/com/ngins/spring/jpa/postgresql/jpa/repository/RoleGroupRepositiry.java
+++ src/main/java/com/ngins/spring/jpa/postgresql/jpa/repository/RoleGroupRepositiry.java
@@ -6,11 +6,11 @@ package com.ngins.spring.jpa.postgresql.jpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.ngins.spring.jpa.postgresql.jpa.entity.AuditLog;
+import com.ngins.spring.jpa.postgresql.jpa.entity.RoleGroup;
 /**
  * 
  */
 @Repository
-public interface AuditLogRepositiry extends JpaRepository<AuditLog, Long> {
+public interface RoleGroupRepositiry extends JpaRepository<RoleGroup, Long> {
 
 }
