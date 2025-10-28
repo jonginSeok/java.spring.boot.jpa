a912865e036a61342ce9ca2852d8dbc055c0cd86
JonginSeok <ngins7512@gmail.com>
2025-10-29 04:27:39 +0900

    update

--- src/main/java/com/ngins/spring/jpa/postgresql/config/SecurityConfig.java
+++ src/main/java/com/ngins/spring/jpa/postgresql/config/SecurityConfig.java
@@ -1,19 +1,14 @@
 package com.ngins.spring.jpa.postgresql.config;
 
-import org.springframework.boot.security.oauth2.server.resource.autoconfigure.OAuth2ResourceServerProperties.Jwt;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.SecurityFilterChain;
-
-import ch.qos.logback.core.pattern.Converter;
 
 @Configuration
 @EnableWebSecurity
